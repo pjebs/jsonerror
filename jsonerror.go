961e60cdfea1f4899191a09d95b637c7d8220b07
pjebs <enquiries@pjebs.com.au>
2015-02-11 21:53:54 +1100

    Errata

--- jsonerror.go
+++ jsonerror.go
@@ -6,12 +6,12 @@ import (
 
 type JSONError struct {
 	Code    int
+	Domain  string
 	error   string
 	message string
-	Domain  string
 }
 
-//Creates a new JSONError struct
+//Creates a new JSONError struct.
 //Domain is optional but can be at most 1 string.
 func New(code int, error string, message string, domain ...string) *JSONError {
 	if len(domain) == 0 {
@@ -41,7 +41,7 @@ func (self JSONError) Error() string {
 	return finalString
 }
 
-//For use with package: gopkg.in/unrolled/render.v1
+//For use with package: "gopkg.in/unrolled/render.v1".
 //Can easily output properly formatted JSON error messages for REST API services.
 func (self JSONError) Render() map[string]string {
 
